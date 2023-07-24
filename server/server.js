1a190603ae16f5334a3729c2faf65a3b551742ea
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-07-24 23:16:46 +0530

    Setting up the middleware and models

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
+require('dotenv').config()
 const express = require('express')
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 8080;
 const root = require('./routes/root')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 
+connectDB()
+app.use(cors())
+app.use(express.json())
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-console.log(path.join(__dirname, 'public'))
-app.use('/',root)
-app.listen(PORT,() => {
+app.use('/', root)
+
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => {
     console.log(`Server running at PORT ${PORT}`)
+})
+})
+
+
+mongoose.connection.on('error', (err) => {
+    console.log(err)
 })
