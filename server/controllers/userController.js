9c7c4594a50cd40d3cb909c24f0f15e72f2d4da1
Suman Bhandari <sumanbhandari9860@gmail.com>
2023-08-17 13:04:44 +0530

    commit

---
+++ server/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
+
+
+const getAllUsers = asyncHandler(async (req,res) => {
+    
+})
+const createNewUser = asyncHandler(async (req,res) => {
+    
+})
+
+const updateUser = asyncHandler(async (req,res) => {
+    
+})
+
+const deleteUser = asyncHandler(async (req,res) => {
+    
+})
+
+
+module.exports =  {getAllUsers,createNewUser,updateUser,deleteUser}
